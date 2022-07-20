057f529ff38525a041b7bb45995357bb43a4d484
salvin308 <salvinsebastian308@gmail.com>
2022-07-20 10:43:19 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,111 +1,308 @@
 class script(object):
-    START_TXT = """HELLO {},
-MY NAME IS <a href=https://t.me/{}>{}</a>, I AM WORK FOR @movieclub308 . MANAGED BY @salvinsebastian308"""
-    HELP_TXT = """𝙷𝙴𝚈 {}
-HERE IS THE HELP FOR MY COMMANDS."""
-    ABOUT_TXT = """✮ MY NAME: {}
-✮ CREATOR: <a href=https://t.me/salvinsebastian308>SALVIN</a>
+    START_TXT = """<b>HELLO {},
+MY NAME IS <a href=https://t.me/{}>{}</a>, I AM A ⚡ POWERFUL AUTOFILTER BOT ⚡. HERE YOU CAN GET ALL MOVIES\n\nWORK FOR @SS_Movie_Club
+MANAGED BY @SS_ADMIN_308_bot</b>"""
+    HELP_TXT = """<b>𝙷𝙴𝚈 {}
+HERE IS THE HELP FOR MY COMMANDS.</b>"""
+    ABOUT_TXT = """<b>✮ MY NAME: {}
+✮ CREATOR: <a href=https://t.me/SS_ADMIN_308_bot>SALVIN</a>
 ✮ LIBRARY: PYROGRAM
 ✮ LANGUAGE: PYTHON 3
 ✮ DATA BASE: MONGO DB
 ✮ BOT SERVER: HEROKU
-
 <b>SUPPORT:</b>
-- <a href=https://t.me/SS_Admin_Chat_bot>SS Admin Chat Bot</a>"""
+- <a href=https://t.me/SS_ADMIN_308_bot>SS Admin Chat Bot</a></b>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
+›› <b>Donation</b>
+⪼ <b>You Can Donate Any Amount You Have 💳. 
+<b>━━━━━━━━━᚜ Payment Methods ᚛━━━━━━━━━
+✮ GooglePay
+✮ Paytm
+✮ Phonepe
+_Contact Me For Know About The Payment Info_
+━━━━━━━━━━━━᚜ <a href=https://t.me/SS_ADMIN_308_bot><b>SALVIN</b></a> ᚛━━━━━━━━━━━━
+›› <b>Paid Promotion</b>
+⪼ <b>Contact Me With You Content Which You Want To Promote . 
+<b>━━━━━━━━━᚜ Payment Methods ᚛━━━━━━━━━
+✮ GooglePay
+✮ Paytm
+✮ Phonepe
+_Contact Me With You Content AND KNOW ABOUT THE PAYMENT INFO_
+━━━━━━━━━━━━᚜ <a href=https://t.me/SS_ADMIN_308_bot><b>SALVIN</b></a> ᚛━━━━━━━━━━━━"""
+    PROMOTION_TXT = """<b>〄 Paid Promotion 〄</b>
+⪼ <b>Contact Me With You Content Which You Want To Promote . 
+<b>━━━━━━━━━᚜ Payment Methods ᚛━━━━━━━━━
+✮ GooglePay
+✮ Paytm
+✮ Phonepe
+_Contact Me With You Content Which You Want To Promote_
+━━━━━━━━━━━━᚜ <a href=https://t.me/SS_ADMIN_308_bot><b>SALVIN</b></a> ᚛━━━━━━━━━━━━""" 
+    FILE_TXT = """➤ HELP: FILE STORE MODULE../
+<b>BY USING THIS MODULE YOU CAN STORE FILES IN MY DATABASE AND I WILL GIVE YOU A PERMANENT LINK TO ACCESS THE SAVED FILES. IF YOU WANT TO ADD FILES FROM A PUBLIC CHANNEL SEND THE FILW LINK ONLY OR YOU WANT TO ADD FILES FROM A PRIVATE CHANNEL YOU MUST MAKE ME ADMIN ON THE CHANNEL TO ACCESS FILES...//</b>
+⪼ Commands and Usage ›
+➪ /plink ›› <b>REPLY TO ANY MEDIA TO GET LINK.</b>
+➪ /pbatch ›› <b>USE YOUR MEDIA LINK WITH THIS COMMAND.</b>
+➪ /batch ›› <b>TO CREATE LINK FOR MULTIPLE FILES.</b>
+⪼ Example ›
+<code>/batch https://t.me/SS_OTT_Channel/5 https://t.me/SS_OTT_Channel/8</code>
+𝙲𝚁𝙴𝙳𝙸𝚃𝚂 ›› <a href=https://t.me/movie_club_308><b>SS MOVIE CLUB</b></a>"""
+    WHOIS_TXT ="""<b>WHO IS MODULE</b>
+Note:- Give a user details
+•/whois :-give a user full details"""
+    FUN_TXT ="""<b>Gᴀᴍᴇs</b> 
+    
+<b>⚡ JUST SOME KIND OF FUN THING'S ⚡</b>
+ 
+𝟣. /dice - ROLE THE DICE
+𝟤. /Throw 𝗈𝗋 /Dart - TO MAKE DART 
+3. /Runs - SOME RAMDOM DIALOGUES
+4. /Goal or /Shoot - TO MAKE A GOAL OR SHOOT
+5. /luck or /cownd - SPIN AND TRY YOUR LUCK"""
 
-- Filter is the feature were users can set automated replies for a particular keyword and SANTO will respond whenever a keyword is found the message
-
+    MANUELFILTER_TXT = """Help: <b>Filters</b>
+- Filter is the feature were users can set automated replies for a particular keyword and SANTO  will respond whenever a keyword is found the message
 <b>NOTE:</b>
-1. bot should have admin privillage.
+1. SANTO should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
-
 <b>Commands and Usage:</b>
-➾ /filter - <code>add a filter in chat</code>
-➾ /filters - <code>list all the filters of a chat</code>
-➾ /del - <code>delete a specific filter in chat</code>
-➾ /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
+• /filter - <code>add a filter in chat</code>
+• /filters - <code>list all the filters of a chat</code>
+• /del - <code>delete a specific filter in chat</code>
+• /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
+    SONG_TXT = """<b>𝚂𝙾𝙽𝙶 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳 𝙼𝙾𝙳𝚄𝙻𝙴</b>
+<b>SONGE DOWNLOAD MODULE, FOR THOSE WHO LOVE MUSIC. YOU CAN USE THIS FEATUE FOR DOWNLOAD ANY SONG WITH SUPER FAST SPEED./</b>
+<b>𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂</b>
+››  /song SONG NAME
+WORK ONLY ON GROUP
+CREDITS:- <a href=https://t.me/SS_Movie_Club</a>"""
+    PIN_TXT ="""<b>PIN MODULE</b>
+<b>PIN A MESSAGE../</b>
+<b>ALL THE PIN RELATED COMMANDS CAN BE FOUND HERE::</b>
+<b>📌 COMMANDS AND USAGE 📌</b>
+◉ /pin :- TO PIN THE MESSAGE ON YOUR CHATS
+◉ /unpin :- TO UNPIN THE CURREENT PINNED MESSAGE"""
+    PASTE_TXT = """Help: <b>Paste</b>
+Paste some texts or documents on a website!
+<b>Commands and Usage:</b>
+• /paste [text] - paste the given text on Pasty
+<b>NOTE:</b>
+• These commands works on both pm and group.
+• These commands can be used by any group member."""
+    TTS_TXT = """Help: <b> TTS 🎤 module:</b>
+Translate text to speech
+<b>Commands and Usage:</b>
+• /tts <text> : convert text to speech
+<b>NOTE:</b>
+• IMDb should have admin privillage.
+• These commands works on both pm and group.
+• IMDb can translate texts to 200+ languages."""
+    PINGS_TXT ="""<b>🌟 Ping:</b>
+Helps you to know your ping 🚶🏼‍♂️
+<b>Commands:</b>
+• /alive - To check you are alive.
+• /help - To get help.
+• /ping - To get your ping.
+• /repo - Source Code.
+• /channel - Channel Details.
+• /ajax - Bot Link.
+<b>🏹Usage🏹 :</b>
+• This commands can be used in pms and groups
+• This commands can be used buy everyone in the groups and bots pm
+• Share us for more features"""
+    TELE_TXT = """<b>▫️HELP: Telegraph▪️</b>
+Do as you wish with telegra.ph module!
+</b>USAGE:</b>
+🤧 /telegraph - Send me Picture or Vide Under (5MB)
+<b>NOTE:</b>
+• This Command Is Available in goups and pms
+• This Command Can be used by everyone"""
+
+    PRIVATEBOT_TXT = """<b>Hello, I am Restarted. Now you can Search movies With Me</b>"""
+
+    JSON_TXT ="""<b>JSON:</b>
+Bot returns json for all replied messages with /json
+<b>Features:</b>
+Message Editting JSON
+Pm Support
+Group Support
+<b>Note:</b>
+Everyone can use this command , if spaming happens bot will automatically ban you from the group."""
+    PURGE_TXT = """<b>Purge</b>
+    
+Delete A Lot Of Messages From Groups! 
+    
+ <b>ADMIN</b> 
+◉ /purge :- Delete All Messages From The Replied To Message, To The Current Message"""
     BUTTON_TXT = """Help: <b>Buttons</b>
-
-- bot Supports both url and alert inline buttons.
-
+-SANTO  Supports both url and alert inline buttons.
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. bot supports buttons with any telegram media type.
+2. SANTO supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
-
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/source00Devil)</code>
-
+<code>[Button Text](buttonurl:https://t.me/movie_club_308)</code>
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
-    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
-
-<b>NOTE:</b>
-1. Make me the admin of your channel if it's private.
-2. make sure that your channel does not contains camrips, porn and fake files.
-3. Forward the last message to me with quotes.
- I'll add all the files in that channel to my db."""
+    AUTOFILTER_TXT = """<b>AUTO FILTER ON/OFF MODULE..</b>
+<b>AUTO FILTER IS THE FEATURE TO FILTER AND SAVE THE FILES AUTOMATICALLY FROM CHANNEL TO GROUP. YOU CAN USE THE FOLLOWING COMMANDS TO ON AND OFF THE AUTOFILTER IN YOUR GROUP.../</b>
+<b>𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 :-
+<b>›› /autofilter on - ENABLE AUTO FILTER IN THE GROUPS.</b>
+<b>›› /autofilter off - DISABLED AUTO FILTER IN THE GROUPS.</b>
+<b>›› /set_template - SET CUSTOM IMDB TEMPLETE FOR AUTO FILTER.</b>
+<b>›› /get_template - GET CURRENT IMDB TEMPLETE OF AUTO FILTER.</b>
+<b>𝙲𝚁𝙴𝙳𝙸𝚃𝚂 :- <a href=https://t.me/movie_club_308</a></b>"""
     CONNECTION_TXT = """Help: <b>Connections</b>
-
 - Used to connect bot to PM for managing filters 
 - it helps to avoid spamming in groups.
-
 <b>NOTE:</b>
 1. Only admins can add a connection.
 2. Send <code>/connect</code> for connecting me to ur PM
-
 <b>Commands and Usage:</b>
-➾ /connect  - <code>connect a particular chat to your PM</code>
-➾ /disconnect  - <code>disconnect from a chat</code>
-➾ /connections - <code>list all your connections</code>"""
+• /connect  - <code>connect a particular chat to your PM</code>
+• /disconnect  - <code>disconnect from a chat</code>
+• /connections - <code>list all your connections</code>"""
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
-
 <b>NOTE:</b>
-These are the extra features of SANTO
-
+these are the extra features of AUTOFILTERBOT 
 <b>Commands and Usage:</b>
-➾ /id - <code>get id of a specifed user.</code>
-➾ /info  - <code>get information about a user.</code>
-➾ /imdb  - <code>get the film information from IMDb source.</code>
-➾ /search  - <code>get the film information from various sources.</code>"""
+• /id - <code>get id of a specifed user.</code>
+• /info  - <code>get information about a user.</code>
+• /imdb  - <code>get the film information from IMDb source.</code>
+• /search  - <code>get the film information from various sources.</code>"""
     ADMIN_TXT = """Help: <b>Admin mods</b>
-
 <b>NOTE:</b>
-This module only works for my OWNER⚡
-
+This module only works for my admins
 <b>Commands and Usage:</b>
-➾ /logs - <code>to get the rescent errors</code>
-➾ /stats - <code>to get status of files in db.</code>
-➾ /delete - <code>to delete a specific file from db.</code>
-➾ /users - <code>to get list of my users and ids.</code>
-➾ /chats - <code>to get list of the my chats and ids </code>
-➾ /leave  - <code>to leave from a chat.</code>
-➾ /disable  -  <code>do disable a chat.</code>
-➾ /ban  - <code>to ban a user.</code>
-➾ /unban  - <code>to unban a user.</code>
-➾ /channel - <code>to get list of total connected channels</code>
-➾ /broadcast - <code>to broadcast a message to all users</code>
-➾ /setttings - <code>to open the settings in PM</code>"""
-    STATUS_TXT = """✮ TOTAL FILES: <code>{}</code>
-✮ TOTAL USERS: <code>{}</code>
-✮ TOTAL CHATS: <code>{}</code>
-✮ USED STORAGE: <code>{}</code> 𝙼𝚒𝙱
-✮ FREE STORAGE: <code>{}</code> 𝙼𝚒𝙱"""
-    LOG_TEXT_G = """#NewGroup
-✮ GROUP ›› {}(<code>{}</code>)
-✮ TOTAL MEMBERS ›› <code>{}</code>
-✮ ADDED BY ›› {}
+• /logs - <code>to get the rescent errors</code>
+• /stats - <code>to get status of files in db.</code>
+• /delete - <code>to delete a specific file from db.</code>
+• /users - <code>to get list of my users and ids.</code>
+• /chats - <code>to get list of the my chats and ids </code>
+• /leave  - <code>to leave from a chat.</code>
+• /disable  -  <code>do disable a chat.</code>
+• /ban_user  - <code>to ban a user.</code>
+• /unban_user  - <code>to unban a user.</code>
+• /channel - <code>to get list of total connected channels</code>
+• /broadcast - <code>to broadcast a message to all users</code>"""
+    STATUS_TXT = """<b>᚛› TOTAL FILES: <code>{}</code></b>
+<b>᚛› TOTAL USERS: <code>{}</code></b>
+<b>᚛› TOTAL CHATS: <code>{}</code></b>
+<b>᚛› USED STORAGE: <code>{}</code></b>
+<b>᚛› FREE STORAGE: <code>{}</code></b>"""
+    LOG_TEXT_G = """#New_Group
+    
+<b>᚛› GROUP ⪼ {}(<code>{}</code>)</b>
+<b>᚛› TOTAL MEMBERS ⪼ <code>{}</code></b>
+<b>᚛› ADDED BY ⪼ {}</b>
 """
-    LOG_TEXT_P = """#New User
-✮ ID ›› <code>{}</code>
-✮ NAME ›› {}
+    LOG_TEXT_P = """#New_User
+    
+<b>᚛› ID - <code>{}</code></b>
+<b>᚛› NAME - {}</b>
 """
-    CARBON_TXT = """ <b>CARBON MODULE</b>
-<b>YOU CAN BEAUTIFY YOUR CODES BY USING THE FEATURE..............</b>
-<b>COMMAND.!</b>
-<b>/carbon ›› REPLY TO ANY TEXT MESSAGE</b>
+    REPORT_TXT = """➤ HELP: <code>Report Sended To My Admin</code> ⚠️
+𝚃𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚑𝚎𝚕𝚙𝚜 𝚢𝚘𝚞 𝚝𝚘 𝚛𝚎𝚙𝚘𝚛𝚝 𝚊 𝚖𝚎𝚜𝚜𝚊𝚐𝚎 𝚘𝚛 𝚊 𝚞𝚜𝚎𝚛 𝚝𝚘 𝚝𝚑𝚎 𝚊𝚍𝚖𝚒𝚗𝚜 𝚘𝚏 𝚝𝚑𝚎 𝚛𝚎𝚜𝚙𝚎𝚌𝚝𝚒𝚟𝚎 𝚐𝚛𝚘𝚞𝚙. 𝙳𝚘𝚗'𝚝 𝚖𝚒𝚜𝚞𝚜𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍.
+➤ Commands and Usage:
+➪/report 𝗈𝗋 @admins - 𝖳𝗈 𝗋𝖾𝗉𝗈𝗋𝗍 𝖺 𝗎𝗌𝖾𝗋 𝗍𝗈 𝗍𝗁𝖾 𝖺𝖽𝗆𝗂𝗇𝗌 (𝗋𝖾𝗉𝗅𝗒 𝗍𝗈 𝖺 𝗆𝖾𝗌𝗌𝖺𝗀𝖾)."""
 
-<b>WORK ON BOTH GROUP AND PM</b>
-<b>CREDITS ››</b> <a href=https://t.me/movieclub308>👥 SS Movie Club 👥</a></b>"""
+    CORONA_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝖢𝗈𝗏𝗂𝖽
+𝚃𝚑𝚒𝚜 𝙲𝚘𝚖𝚖𝚊𝚗𝚍 𝚑𝚎𝚕𝚙𝚜 𝚢𝚘𝚞 𝚝𝚘 𝚔𝚗𝚘𝚠 𝚍𝚊𝚒𝚕𝚢 𝚒𝚗𝚏𝚘𝚛𝚖𝚊𝚝𝚒𝚘𝚗 𝚊𝚋𝚘𝚞𝚝 𝚌𝚘𝚟𝚒𝚍 
+➤ Commands and Usage:
+➪ /covid - 𝗎𝗌𝖾 𝗍𝗁𝗂𝗌 𝖼𝗈𝗆𝗆𝖺𝗇𝖽 𝗐𝗂𝗍𝗁 𝗒𝗈𝗎𝗋 𝖼𝗈𝗎𝗇𝗍𝗋𝗒 𝗇𝖺𝗆𝖾 𝗍𝗈 𝗀𝖾𝗍 𝖼𝗈𝗏𝗂𝖽𝖾 𝗂𝗇𝖿𝗈𝗋𝗆𝖺𝗍𝗂𝗈𝗇
+➛𝖤𝗑𝖺𝗆𝗉𝗅𝖾:
+<code>/covid 𝖨𝗇𝖽𝗂𝖺</code>"""
+
+    URLSHORT_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝖴𝗋𝗅 𝗌𝗁𝗈𝗋𝗍𝗇𝖾𝗋
+𝚃𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚑𝚎𝚕𝚙𝚜 𝚢𝚘𝚞 𝚝𝚘 𝚜𝚑𝚘𝚛𝚝 𝚊 𝚞𝚛𝚕 
+➤ Commands and Usage:
+➪ /short: 𝗎𝗌𝖾 𝗍𝗁𝗂𝗌 𝖼𝗈𝗆𝗆𝖺𝗇𝖽 𝗐𝗂𝗍𝗁 𝗒𝗈𝗎𝗋 𝗅𝗂𝗇𝗄 𝗍𝗈 𝗀𝖾𝗍 𝗌𝗁𝗈𝗋𝗍𝖾𝖽 𝗅𝗂𝗇𝗄𝗌
+➛𝖤𝗑𝖺𝗆𝗉𝗅𝖾:
+<code>/short https://youtu.be/kB9TkCs8cX0</code>"""
+
+    VIDEO_TXT ="""HELP TO DOWNLOAD VIDEOS FROM YOUTUBE.
+• 𝘜𝘴𝘢𝘨𝘦
+𝘠𝘰𝘶 𝘊𝘢𝘯 𝘋𝘰𝘸𝘯𝘭𝘰𝘢𝘥 𝘈𝘯𝘺 𝘝𝘪𝘥𝘦𝘰 𝘍𝘳𝘰𝘮 𝘠𝘰𝘶𝘵𝘶𝘣𝘦
+How to Use
+• 𝘛𝘺𝘱𝘦 /video or /mp4 𝘈𝘯𝘥 (https://youtu.be/kB9TkCs8cX0)
+• 𝘌𝘹𝘢𝘮𝘱𝘭𝘦:
+<code>/mp4 https://youtu.be/kB9TkCs8cX0</code>
+<code>/video https://youtu.be/kB9TkCs8cX0</code>"""
+
+    ZOMBIES_TXT = """HELP YOU TO KICK USERS
+<b>Kick incative members from group. Add me as admin with ban users permission in group.</b>
+<b>Commands and Usage:</b>
+• /inkick - command with required arguments and i will kick members from group.
+• /instatus - to check current status of chat member from group.
+• /inkick within_month long_time_ago - to kick users who are offline for more than 6-7 days.
+• /inkick long_time_ago - to kick members who are offline for more than a month and Deleted Accounts.
+• /dkick - to kick deleted accounts."""
+
+    IMAGE_TXT = """➤ Help: Iᴍᴀɢᴇ
+𝚃𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚑𝚎𝚕𝚙𝚜 𝚢𝚘𝚞 𝚝𝚘 𝚎𝚍𝚒𝚝 𝚒𝚖𝚊𝚐𝚎 𝚟𝚎𝚛𝚢 𝚎𝚊𝚜𝚒𝚕𝚢 
+➤ Commands and Usage:
+➪ 𝖩𝗎𝗌𝗍 𝗌𝖾𝗇𝖽 𝗆𝖾 𝖺 𝗂𝗆𝖺𝗀𝖾 𝗍𝗈 𝖾𝖽𝗂𝗍 ✨
+𝖬𝖺𝖽𝖾 𝖻𝗒 <a href=https://t.me/SS_ADMIN_308_bot>SALVIN</a>"""
+
+    STICKER_TXT = """YOU CAN USE THIS MODULE TO FIND ANY STICKER.
+• USAGE
+To Get Sticker ID
+ 
+  ⭕ How to use
+ 
+◉ Reply To Any Sticker [/stickerid]"""
+
+    YTTHUMB_TXT = """HELPS TO DOWNLOAD ANY YOUTUBE VIDEO THUMBNAIL
+    
+⭕ How to use
+𝘛𝘺𝘱𝘦 /ytthumb 𝘈𝘯𝘥 𝘝𝘪𝘥𝘦𝘰 𝘓𝘪𝘯𝘬
+• 𝘌𝘹𝘢𝘮𝘱𝘭𝘦
+<code>/ytthumb https://youtu.be/UyzJ9KEoU0w</code>"""
+
+    ABOOK_TXT = """➤ Help: 𝖠𝗎𝖽𝗂𝗈𝖻𝗈𝗈𝗄
+𝚈𝚘𝚞 𝚌𝚊𝚗 𝚌𝚘𝚗𝚟𝚎𝚛𝚝 𝚊 𝙿𝙳𝙵 𝚏𝚒𝚕𝚎 𝚝𝚘 𝚊 𝚊𝚞𝚍𝚒𝚘 𝚏𝚒𝚕𝚎 𝚠𝚒𝚝𝚑 𝚝𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 ✯
+➤ Commands and Usage:
+➪ /audiobook: 𝖱𝖾𝗉𝗅𝗒 𝗍𝗁𝗂𝗌 𝖼𝗈𝗆𝗆𝖺𝗇𝖽 𝗍𝗈 𝖺𝗇𝗒 𝖯𝖣𝖥 𝗍𝗈 𝗀𝖾𝗇𝖾𝗋𝖺𝗍𝖾 𝗍𝗁𝖾 𝖺𝗎𝖽𝗂𝗈"""
+
+    GTRANS_TXT = """➤ Help: 𝖦𝗈𝗈𝗀𝗅𝖾 𝖳𝗋𝖺𝗇𝗌𝗅𝖺𝗍𝖾𝗋
+𝚃𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚑𝚎𝚕𝚙𝚜 𝚢𝚘𝚞 𝚝𝚘 𝚝𝚛𝚊𝚗𝚜𝚕𝚊𝚝𝚎 𝚊 𝚝𝚎𝚡𝚝 𝚝𝚘 𝖺𝗇𝗒 𝚕𝚊𝚗𝚐𝚞𝚊𝚐𝚎𝚜 𝚢𝚘𝚞 𝚠𝚊𝚗𝚝. 𝚃𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚠𝚘𝚛𝚔𝚜 𝚘𝚗 𝚋𝚘𝚝𝚑 𝚙𝚖 𝚊𝚗𝚍 𝚐𝚛𝚘𝚞𝚙 ✯
+➤ Commands and Usage:
+➪/tr - 𝖳𝗈 𝗍𝗋𝖺𝗇𝗌𝗅𝖺𝗍𝖾𝗋 𝗍𝖾𝗑𝗍𝗌 𝗍𝗈 𝖺 𝗌𝗉𝖾𝖼𝗂𝖿𝖼 𝗅𝖺𝗇𝗀𝗎𝖺𝗀𝖾
+➤ 𝖭𝗈𝗍𝖾:
+𝖶𝗁𝗂𝗅𝖾 𝗎𝗌𝗂𝗇𝗀 /tr 𝗒𝗈𝗎 𝗌𝗁𝗈𝗎𝗅𝖽 𝗌𝗉𝖾𝖼𝗂𝖿𝗒 𝗍𝗁𝖾 𝗅𝖺𝗇𝗀𝗎𝖺𝗀𝖾 𝖼𝗈𝖽𝖾
+➛𝖤𝗑𝖺𝗆𝗉𝗅𝖾: /𝗍𝗋 𝗆𝗅
+• 𝖾𝗇 = 𝖤𝗇𝗀𝗅𝗂𝗌𝗁
+• 𝗆𝗅 = 𝖬𝖺𝗅𝖺𝗒𝖺𝗅𝖺𝗆
+• 𝗁𝗂 = 𝖧𝗂𝗇𝖽𝗂"""
+
+    RESTRIC_TXT = """➤ Help: Mᴜᴛᴇ 🚫
+𝚃𝚑𝚎𝚜𝚎 𝚊𝚛𝚎 𝚝𝚑𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚊 𝚐𝚛𝚘𝚞𝚙 𝚊𝚍𝚖𝚒𝚗 𝚌𝚊𝚗 𝚞𝚜𝚎 𝚝𝚘 𝚖𝚊𝚗𝚊𝚐𝚎 𝚝𝚑𝚎𝚒𝚛 𝚐𝚛𝚘𝚞𝚙 𝚖𝚘𝚛𝚎 𝚎𝚏𝚏𝚒𝚌𝚒𝚎𝚗𝚝𝚕𝚢.
+➪/ban: 𝖳𝗈 𝖻𝖺𝗇 𝖺 𝗎𝗌𝖾𝗋 𝖿𝗋𝗈𝗆 𝗍𝗁𝖾 𝗀𝗋𝗈𝗎𝗉.
+➪/unban: 𝖳𝗈 𝗎𝗇𝖻𝖺𝗇 𝖺 𝗎𝗌𝖾𝗋 𝗂𝗇 𝗍𝗁𝖾 𝗀𝗋𝗈𝗎𝗉.
+➪/tban: 𝖳𝗈 𝗍𝖾𝗆𝗉𝗈𝗋𝖺𝗋𝗂𝗅𝗒 𝖻𝖺𝗇 𝖺 𝗎𝗌𝖾𝗋.
+➪/mute: 𝖳𝗈 𝗆𝗎𝗍𝖾 𝖺 𝗎𝗌𝖾𝗋 𝗂𝗇 𝗍𝗁𝖾 𝗀𝗋𝗈𝗎𝗉.
+➪/unmute: 𝖳𝗈 𝗎𝗇𝗆𝗎𝗍𝖾 𝖺 𝗎𝗌𝖾𝗋 𝗂𝗇 𝗍𝗁𝖾 𝗀𝗋𝗈𝗎𝗉.
+➪/tmute: 𝖳𝗈 𝗍𝖾𝗆𝗉𝗈𝗋𝖺𝗋𝗂𝗅𝗒 𝗆𝗎𝗍𝖾 𝖺 𝗎𝗌𝖾𝗋.
+➤ 𝖭𝗈𝗍𝖾:
+𝖶𝗁𝗂𝗅𝖾 𝗎𝗌𝗂𝗇𝗀 /tmute 𝗈𝗋 /tban 𝗒𝗈𝗎 𝗌𝗁𝗈𝗎𝗅𝖽 𝗌𝗉𝖾𝖼𝗂𝖿𝗒 𝗍𝗁𝖾 𝗍𝗂𝗆𝖾 𝗅𝗂𝗆𝗂𝗍.
+➛𝖤𝗑𝖺𝗆𝗉𝗅𝖾: /𝗍𝖻𝖺𝗇 2𝖽 𝗈𝗋 /𝗍𝗆𝗎𝗍𝖾 2𝖽.
+𝖸𝗈𝗎 𝖼𝖺𝗇 𝗎𝗌𝖾 𝗏𝖺𝗅𝗎𝖾𝗌: 𝗆/𝗁/𝖽. 
+ • 𝗆 = 𝗆𝗂𝗇𝗎𝗍𝖾𝗌
+ • 𝗁 = 𝗁𝗈𝗎𝗋𝗌
+ • 𝖽 = 𝖽𝖺𝗒𝗌"""
+    CREATOR_REQUIRED = """❗<b>You have To Be The Group Creator To Do That.</b>"""
+      
+    INPUT_REQUIRED = "❗ **Arguments Required**"
+      
+    KICKED = """✔️ Successfully Kicked {} Members According To The Arguments Provided."""
+      
+    START_KICK = """🚮 Removing Inactive Members This May Take A While..."""
+      
+    ADMIN_REQUIRED = """❗<b>I Want to be an admin in this group bye.. Add Me Again with all admin rights.</b>"""
+      
+    DKICK = """✔️ Kicked {} Deleted Accounts Successfully."""
+      
+    FETCHING_INFO = """<b>Copying files from that Channel. Please Wait...</b>"""
+      
+    STATUS = """{}\n<b>Chat Member Status</b>**\n\n```<i>Recently``` - {}\n```Within Week``` - {}\n```Within Month``` - {}\n```Long Time Ago``` - {}\nDeleted Account - {}\nBot - {}\nUnCached - {}</i>
+"""
