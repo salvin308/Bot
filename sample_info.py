78e4d51a0ca1a53ddcaebcc07864e0241d4aa3eb
salvin308 <salvinsebastian308@gmail.com>
2022-07-20 10:37:49 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,4 +1,3 @@
-# Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
 API_ID = 12345
@@ -7,7 +6,7 @@ BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
-CACHE_TIME = 300
+CACHE_TIME = 100
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
@@ -19,6 +18,6 @@ AUTH_CHANNEL = None
 # MongoDB information
 DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
 DATABASE_NAME = 'Telegram'
-COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+COLLECTION_NAME = 'channel_files'
 
 
