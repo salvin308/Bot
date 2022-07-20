e83201fd6a5d5c4db529ce8c1c7312f1d03540b1
salvin308 <salvinsebastian308@gmail.com>
2022-07-20 10:53:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Ajax.git /Ajax
+  git clone https://github.com/salvin308/bot.git /bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /bot
 fi
-cd /Ajax
+cd /bot
 pip3 install -U -r requirements.txt
 echo "Starting SPIDEY....🔥"
 python3 bot.py
