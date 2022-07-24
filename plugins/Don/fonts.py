f27af1c36252c780a004d8e13abf171338c98399
salvin308 <salvinsebastian308@gmail.com>
2022-07-24 20:09:49 +0530

    Update fonts.py

--- plugins/Don/fonts.py
+++ plugins/Don/fonts.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery 
-from plugins.functions import Fonts
+from fonts_func.functions import Fonts
 
 @Client.on_message(filters.command("font"))
 async def style_buttons(bot, update, cb=False):
