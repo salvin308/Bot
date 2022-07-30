ece853aa2b307f28158feb20e6f7b8c7fce759ba
salvin308 <salvinsebastian308@gmail.com>
2022-07-30 21:02:24 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -139,6 +139,8 @@ async def re_enable_chat(bot, message):
 @Client.on_message(filters.command('stats') & filters.incoming)
 async def get_ststs(bot, message):
     rju = await message.reply_sticker("CAACAgUAAxkBAAIYe2LkBCVSAAGAuRcB0N8ls0XEkwiTGAACmgADyJRkFCxl4eFc7yVqHgQ")
+    await asyncio.sleep(2)
+    await rju.delete()
     total_users = await db.total_users_count()
     totl_chats = await db.total_chat_count()
     files = await Media.count_documents()
