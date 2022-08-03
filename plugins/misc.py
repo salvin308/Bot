25f1f4fdd4815d5b1ae6fc49f45baa509667f9ea
salvin308 <salvinsebastian308@gmail.com>
2022-08-03 18:41:34 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -69,7 +69,7 @@ async def aboutme(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.ABOUT_TXT.format(message.from_user.mention),
+            caption=script.ABOUT_TXT.format(message.chat.title),
             reply_markup=reply_markup,
             parse_mode='html'
         )
