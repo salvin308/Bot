959000abe033fa1ff4777153e0cbee06e5a20589
salvin308 <salvinsebastian308@gmail.com>
2022-08-26 06:34:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -64,7 +64,7 @@ MAX_MESSAGE_LENGTH = 4096
 TMP_DOWNLOAD_DIRECTORY = environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
 
 # Data base channel AutoCaption Option
-DATABASE_CHANNEL_FILE_CAPTION = os.environ.get("DATABASE_CHANNEL_FILE_CAPTION", "`{file_name}`")
+DATABASE_CHANNEL_FILE_CAPTION = os.environ.get("DATABASE_CHANNEL_FILE_CAPTION", "{file_name}")
 
 # the maximum number of 'selectable' messages in Telegram
 TG_MAX_SELECT_LEN = environ.get("TG_MAX_SELECT_LEN", "100")
